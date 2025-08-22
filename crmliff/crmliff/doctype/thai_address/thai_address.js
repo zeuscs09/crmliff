4028c35504b8d81169bbb4a7e786b87d51b33960
tonjingjai <zeuscs09@gmail.com>
2025-08-22 09:29:47 +0700

    ปรับปรุง Postman collection สำหรับ CRM LIFF API โดยเพิ่มฟังก์ชันการจัดการ pagination และการกรองข้อมูลสำหรับรายการร้านค้าและพนักงานเซลส์ รวมถึงการปรับปรุงคำอธิบายและโครงสร้างของคำขอ API ให้ชัดเจนยิ่งขึ้น

---
+++ crmliff/crmliff/doctype/thai_address/thai_address.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, nattapong@jingjaisoft.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Thai Address", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ crmliff/crmliff/doctype/thai_address/thai_address.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, nattapong@jingjaisoft.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ThaiAddress(Document):
+	pass
